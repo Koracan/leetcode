fe1dc1e11f30324026f01b09d7418f3d83b0c24e
koracan <koracan@163.com>
2026-04-25 17:05:48 +0800

    458

--- editor/cn/[3539]魔法序列的数组乘积之和.cs
+++ editor/cn/[3539]魔法序列的数组乘积之和.cs
@@ -70,14 +70,76 @@
 //
 // Related Topics 位运算 数组 数学 动态规划 状态压缩 组合数学 👍 3 👎 0
 
+using System.Numerics;
+
 namespace FindSumOfArrayProductOfMagicalSequences;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 public class Solution
 {
+    private const long Mod = 1_000_000_007;
     public int MagicalSum(int m, int k, int[] nums)
     {
-        throw new NotImplementedException();
+        var n = nums.Length;
+        var fac = new long[m + 1];
+        fac[0] = 1;
+        for (int i = 1; i <= m; i++)
+            fac[i] = fac[i - 1] * i % Mod;
+
+        var ifac = new long[m + 1];
+        ifac[0] = ifac[1] = 1;
+        for (int i = 2; i <= m; i++)
+            ifac[i] = ModReverse(i);
+        for (int i = 2; i <= m; i++)
+            ifac[i] = ifac[i - 1] * ifac[i] % Mod;
+
+        var numsPower = new long[n, m + 1];
+        for (int i = 0; i < n; i++) {
+            numsPower[i, 0] = 1;
+            for (int j = 1; j <= m; j++)
+                numsPower[i, j] = numsPower[i, j - 1] * nums[i] % Mod;
+        }
+
+        var f = new long[n, m + 1, m * 2 + 1, k + 1];
+
+        for (int j = 0; j <= m; j++)
+            f[0, j, j, 0] = numsPower[0, j] * ifac[j] % Mod;
+
+        for (int i = 0; i + 1 < n; i++)
+            for (int j = 0; j <= m; j++)
+                for (int p = 0; p <= m * 2; p++)
+                    for (int q = 0; q <= k; q++) {
+                        var q2 = p % 2 + q;
+                        if (q2 > k) break;
+                        for (int r = 0; r + j <= m; r++) {
+                            var p2 = p / 2 + r;
+                            f[i + 1, j + r, p2, q2] += f[i, j, p, q] * numsPower[i + 1, r] % Mod * ifac[r] % Mod;
+                            f[i + 1, j + r, p2, q2] %= Mod;
+                        }
+                    }
+
+        var res = 0L;
+        for (int p = 0; p <= m * 2; p++)
+            for (int q = 0; q <= k; q++)
+                if (BitOperations.PopCount((uint)p) + q == k) {
+                    res += f[n - 1, m, p, q] * fac[m] % Mod;
+                    res %= Mod;
+                }
+
+
+        return (int)res;
+    }
+
+    private static long ModReverse(long x)
+    {
+        var y = Mod - 2;
+        long res = 1, cur = x % Mod;
+        while (y > 0) {
+            if ((y & 1) == 1) res = res * cur % Mod;
+            y >>= 1;
+            cur = cur * cur % Mod;
+        }
+        return res;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
