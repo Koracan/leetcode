14e2453592877eb57ce4a788ed7b893c539280ad
koracan <koracan@163.com>
2026-06-19 13:25:52 +0800

    461

---
+++ editor/cn/[461]汉明距离.cs
@@ -0,0 +1,57 @@
+//两个整数之间的 汉明距离 指的是这两个数字对应二进制位不同的位置的数目。 
+//
+// 给你两个整数 x 和 y，计算并返回它们之间的汉明距离。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：x = 1, y = 4
+//输出：2
+//解释：
+//1   (0 0 0 1)
+//4   (0 1 0 0)
+//       ↑   ↑
+//上面的箭头指出了对应二进制位不同的位置。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：x = 3, y = 1
+//输出：1
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= x, y <= 2³¹ - 1 
+// 
+//
+// 
+//
+// 注意：本题与 2220. 转换数字的最少位翻转次数 相同。 
+//
+// Related Topics 位运算 👍 784 👎 0
+
+namespace HammingDistance;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+public class Solution
+{
+    public int HammingDistance(int x, int y)
+    {
+        var xor = x ^ y;
+        var count = 0;
+        while (xor > 0) {
+            count += xor & 1;
+            xor >>= 1;
+        }
+
+        return count;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
